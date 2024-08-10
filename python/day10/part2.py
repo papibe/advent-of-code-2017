aae9a676c39ba12bb5f4b7a4aa97216a133f8a38
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2024-08-10 18:23:27 -0500

    remove test from main solution

--- python/day10/part2.py
+++ python/day10/part2.py
@@ -97,9 +97,4 @@ def solution(filename: str, size: int) -> str:
 
 
 if __name__ == "__main__":
-    assert solve("", 256) == "a2582a3a0e66e6e86e3812dcb672a272"
-    assert solve("AoC 2017", 256) == "33efeb34ea91902bb2f59c9920caa6cd"
-    assert solve("1,2,3", 256) == "3efbe78a8d82f29979031a4aa0b16a9d"
-    assert solve("1,2,4", 256) == "63960835bcdc130f0b66d7ff4f6a5a8e"
-
     print(solution("./input.txt", 256))  # "35b028fe2c958793f7d5a61d07a008c8"
