b564b4947569f561fb8ef913a1dc8920bccd21da
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2024-08-20 22:08:13 -0500

    add python day 14

---
+++ python/day14/part2.py
@@ -0,0 +1,155 @@
+from collections import deque
+from typing import List, Dict, Tuple, Set, Deque
+
+ONES: Dict[str, int] = {
+    "0": "0000",
+    "1": "0001",
+    "2": "0010",
+    "3": "0011",
+    "4": "0100",
+    "5": "0101",
+    "6": "0110",
+    "7": "0111",
+    "8": "1000",
+    "9": "1001",
+    "a": "1010",
+    "b": "1011",
+    "c": "1100",
+    "d": "1101",
+    "e": "1110",
+    "f": "1111",
+}
+
+
+class Node:
+    def __init__(
+        self,
+        value: int,
+    ) -> None:
+        self.value: int = value
+
+    def set_next(self, next_: "Node") -> None:
+        self.next: Node = next_
+
+    def set_prev(self, prev: "Node") -> None:
+        self.prev: Node = prev
+
+
+class Elements:
+    def __init__(self, size: int) -> None:
+        self.size: int = size
+        self.skip_size: int = 0
+        self.current_postion: Node = Node(value=0)
+        self.head: Node = self.current_postion
+        prev = self.current_postion
+
+        for value in range(1, self.size):
+            n: Node = Node(value)
+            n.set_prev(prev)
+            prev.set_next(n)
+            prev = n
+        prev.set_next(self.current_postion)
+        self.current_postion.set_prev(prev)
+
+    def apply(self, length: int) -> None:
+
+        current: Node = self.current_postion
+        for _ in range(length - 1):
+            current = current.next
+
+        head: Node = self.current_postion
+        tail: Node = current
+
+        while head != tail and head.prev != tail:
+            value: int = head.value
+
+            head.value = tail.value
+            tail.value = value
+
+            head = head.next
+            tail = tail.prev
+
+        current = self.current_postion
+        for _ in range(length + self.skip_size):
+            current = current.next
+
+        self.current_postion = current
+        self.skip_size += 1
+
+
+def parse(filename: str) -> str:
+    with open(filename, "r") as fp:
+        data: str = fp.read().strip()
+
+    return data
+
+
+def get_lengths(data: str) -> List[int]:
+    final_sequence: List[int] = [ord(c) for c in data]
+    final_sequence.extend([17, 31, 73, 47, 23])
+
+    return final_sequence
+
+
+def solve(input_: str, size: int) -> str:
+    lengths: List[int] = get_lengths(input_)
+    elements: Elements = Elements(size)
+
+    for _ in range(64):
+        for length in lengths:
+            elements.apply(length)
+
+    dense_hash: List[int] = []
+    current: Node = elements.head
+    for _ in range(16):
+        xor: int = 0
+        for _ in range(16):
+            xor ^= current.value
+            current = current.next
+        dense_hash.append(xor)
+
+    return "".join([f"{n:0{2}x}" for n in dense_hash])
+
+
+def solution(input_: str, size: int) -> str:
+    grid: List[str] = []
+    for row_number in range(128):
+        row_hash: str = solve(input_ + f"-{row_number}", size)
+        grid_row = []
+        for char in row_hash:
+             grid_row.append(ONES[char])
+        grid.append("".join(grid_row))
+
+
+    regions: int = 0
+
+    visited: Set[Tuple[int, int]] = set()
+    for row in range(128):
+        for col in range(128):
+            if grid[row][col] == "0" or (row, col) in visited:
+                continue
+
+            # BFS
+            regions += 1
+            start = (row, col)
+            visited.add(start)
+            queue: Deque[Tuple[int, int]] = deque([start])
+
+            while queue:
+                current_row, current_col = queue.popleft()
+                for step_row, step_col in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                    new_row: int = current_row + step_row
+                    new_col: int = current_col + step_col
+                    if 0 <= new_row < 128 and 0 <= new_col < 128:
+                        if grid[new_row][new_col] == "0":
+                            continue
+                        if (new_row, new_col) not in visited:
+                            queue.append((new_row, new_col))
+                            visited.add((new_row, new_col))
+
+    return regions
+
+
+if __name__ == "__main__":
+    print(solution("flqrgnkx", 256))  # 1242
+    print(solution("hxtvlmkl", 256))  # 1093
