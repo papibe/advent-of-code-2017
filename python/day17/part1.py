961037cf586c3cba97bda2e470ad2858beb831be
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2024-09-06 00:22:36 -0500

    Add day 17 both parts

---
+++ python/day17/part1.py
@@ -0,0 +1,64 @@
+import re
+from collections import deque
+from typing import Deque, Dict, List, Match, Optional, Set, Tuple
+
+
+class Node:
+    def __init__(self, value: int) -> None:
+        self.value: int = value
+        self.next: Node = None
+        self.prev: Node = None
+
+class Buffer:
+    def __init__(self, spinlock: int) -> None:
+        node: Node = Node(0)
+        node.next = node
+        node.prev = node
+
+        self.head: Node = node
+        self.size: int = 1
+        self.spinlock: int = spinlock
+        self.zero: Node = node
+
+    def insert(self, value: int) -> None:
+        position: int = self.spinlock % self.size
+        p: Node = self.head
+        for _ in range(position):
+            p = p.next
+
+        node: Node = Node(value)
+        next_: Node = p.next
+        p.next = node
+        node.prev = p
+        node.next = next_
+        next_.prev = node
+
+        self.head = node
+        self.size += 1
+
+    def print(self) -> None:
+        p: Node = self.zero
+        for _ in range(self.size):
+            if p == self.head:
+                print(f"({p.value})", end=" ")
+            else:
+                print(p.value, end=" ")
+            p = p.next
+
+        print()
+
+
+def solution(spinlock: int) -> int:
+    buffer: Buffer = Buffer(spinlock)
+
+    for value in range(1, 2017 + 1):
+    # for value in range(1, 10):
+        buffer.insert(value)
+        # buffer.print()
+
+    return buffer.head.next.value
+
+
+if __name__ == "__main__":
+    print(solution(3))  # 0
+    print(solution(371))  # 0
---
+++ python/day17/part2_v2.py
@@ -0,0 +1,41 @@
+import re
+from collections import deque
+from typing import Deque, Dict, List, Match, Optional, Set, Tuple
+
+
+
+class Buffer:
+    def __init__(self, spinlock: int) -> None:
+
+        self.head_position: int = 0
+        self.size: int = 1
+        self.spinlock: int = spinlock
+        self.zero_position: int = 0
+        self.next_to_zero: int = 0
+
+    def insert(self, value: int) -> None:
+        insert_position: int = self.spinlock % self.size
+
+        if insert_position == self.zero_position:
+            self.next_to_zero = value
+            self.zero_position = self.size
+
+        elif insert_position < self.zero_position:
+            self.zero_position -= insert_position
+
+        elif insert_position > self.zero_position:
+            self.zero_position += self.size - insert_position
+
+        self.size += 1
+
+
+def solution(spinlock: int) -> int:
+    buffer: Buffer = Buffer(spinlock)
+
+    for value in range(1, 50000000 + 1):
+        buffer.insert(value)
+
+    return buffer.next_to_zero
+
+if __name__ == "__main__":
+    print(solution(371))  # 39170601
