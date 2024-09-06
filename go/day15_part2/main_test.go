6fb94383b92c4ac1c95bdc73cbadab24f3896eb9
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2024-09-05 22:44:23 -0500

    Add all go files for day 15 part 2

---
+++ go/day15_part2/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPart1(t *testing.T) {
+	testCases := []struct {
+		a_value  int
+		b_value  int
+		pairs    int
+		expected int
+	}{
+		{65, 8921, 5, 0},
+		{65, 8921, 1056, 1},
+		{65, 8921, NUM_PAIRS, 309},
+	}
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("a starts %d, b starts %d, %d pairs, should be %d", tc.a_value, tc.b_value, tc.pairs, tc.expected), func(t *testing.T) {
+			result := solution(tc.a_value, tc.b_value, tc.pairs)
+			if result != tc.expected {
+				t.Errorf("got %d; want %d", result, tc.expected)
+			}
+		})
+	}
+}
