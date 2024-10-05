d4f2a706579197d86806ee3eefe18b3ae550ef10
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2024-10-05 12:03:34 -0500

    Add python solution for day 23

---
+++ python/day23/part2.py
@@ -0,0 +1,27 @@
+# init registers
+b = c = d = e = f = g = h = 0
+
+# a = 1 causes these values
+b = 108400
+c = 125400
+
+e = 2
+g = 2
+
+while g != 0:
+
+    f = 1
+    d = 2
+
+    for d in range(2, b + 1):
+        # d * e - b == 0
+        if b % d == 0 and 2 <= (b // d) <= b:
+            f = 0
+            break
+    if f == 0:
+        h -= -1
+
+    g = b - c
+    b -= -17
+
+print(h)
