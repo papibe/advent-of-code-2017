e0d310d94a7e2071556c9fb229b5a4cd972608c5
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2024-10-09 21:38:40 -0500

    Add all python files for day 21 part 1

---
+++ python/day21/tests/test_part1.py
@@ -0,0 +1,39 @@
+import pytest
+
+from part1 import INITIAL_ART, Art, get_divisions
+
+art2: Art = [
+    ["#", ".", ".", "#"],
+    [".", ".", ".", "."],
+    [".", ".", ".", "."],
+    ["#", ".", ".", "#"],
+]
+
+art3: Art = [
+    ["#", "#", ".", "#", "#", "."],
+    ["#", ".", ".", "#", ".", "."],
+    [".", ".", ".", ".", ".", "."],
+    ["#", "#", ".", "#", "#", "."],
+    ["#", ".", ".", "#", ".", "."],
+    [".", ".", ".", ".", ".", "."],
+]
+
+
+@pytest.mark.parametrize(
+    "art,expected",
+    [
+        (INITIAL_ART, 1),
+    ],
+)
+def test_3_divisions(art: Art, expected: int) -> None:
+    result: int = len(get_divisions(art, 3))
+    assert result == expected, f"got {result}, needs {expected}"
+
+
+@pytest.mark.parametrize(
+    "art,expected",
+    [(art2, 4), (art3, 9)],
+)
+def test_2_divisions(art: Art, expected: int) -> None:
+    result: int = len(get_divisions(art, 2))
+    assert result == expected, f"got {result}, needs {expected}"
